ecc9eb1937de29127d3ace0852d882271b40a7f4
proclx <i.protzai@gmail.com>
2026-03-10 23:24:05 +0200

    feat: add structured logging with Serilog and Seq

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,18 +15,24 @@ namespace SmartTrip.Controllers
 
         public IActionResult Index()
         {
+            _logger.LogInformation("Home page requested by {UserName} at {RequestTime}",
+                User.Identity?.Name ?? "Anonymous", DateTime.UtcNow);
             return View();
         }
 
         public IActionResult Privacy()
         {
+            _logger.LogInformation("Privacy page accessed by {UserName}", 
+                User.Identity?.Name ?? "Anonymous");
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            _logger.LogError("Error page displayed. RequestId: {RequestId}", requestId);
+            return View(new ErrorViewModel { RequestId = requestId });
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,39 +1,70 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using SmartTrip.Data;
 using SmartTrip.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<SmartTripDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
-builder.Services.AddIdentityApiEndpoints<User>()
-    .AddEntityFrameworkStores<SmartTripDbContext>();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Information("Starting SmartTrip application...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) =>
+        configuration.ReadFrom.Configuration(context.Configuration)
+                     .ReadFrom.Services(services));
+
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    builder.Services.AddDbContext<SmartTripDbContext>(options =>
+        options.UseNpgsql(connectionString));
+
+    builder.Services.AddIdentityApiEndpoints<User>()
+        .AddEntityFrameworkStores<SmartTripDbContext>();
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            diagnosticContext.Set("UserAgent", httpContext.Request.Headers["User-Agent"].ToString());
+        };
+    });
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
